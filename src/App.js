55a76c0de582a09ff595ca52b5c094da307885e5
hoang <minhhoangp@student.unimelb.edu.au>
2022-06-26 19:54:36 +1000

    demo 1 dummy result

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { useEffect } from 'react';
 
+// frontend 
+import './App.css';
+import SearchIcon from './search.svg'
 
 const API_URL = 'https://www.omdbapi.com?apikey=1db74541';
+
+const movie1 = {
+    'Poster': "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+    'Title': "Italian Spiderman",
+    'Type': "movie",
+    'Year': "2007",
+    'imdbID': "tt2705436"
+}
 
 const App = () => {
     // get movie name and return a list of data from OMDBAPI
@@ -18,7 +29,34 @@ const App = () => {
     }, []);
 
     return (
-        <h1>App</h1>
+        <div className='app'>
+            <h1>MovieWorld</h1>
+
+            <div className='search'>
+                <input placeholder='Search for movies' value='Superman' onchange={() => {}}/>
+                <image src = {SearchIcon} alt="search" onClick={() => {}}/>
+            </div>
+
+
+            <div className='container'>
+                <div className='movie'>
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src = {movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                    </div>
+
+                    <div>
+                        <span>{movie1.Type}</span>
+                        <h3>{movie1.Title}</h3>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
     );
 }
 
