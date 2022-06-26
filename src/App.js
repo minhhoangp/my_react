1fbed1f005a12451287d81210ff5c841cd3e97c8
hoang <minhhoangp@student.unimelb.edu.au>
2022-06-26 19:21:54 +1000

    fetch API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+
+const API_URL = 'https://www.omdbapi.com?apikey=1db74541';
 
 const App = () => {
+    // get movie name and return a list of data from OMDBAPI
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        console.log(data);
+    }
+
+    // 
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
     return (
         <h1>App</h1>
     );
